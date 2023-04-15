cbd93c0ff90d5640903cc6c02e003f9cbf07d4d9
Yannick Zwijsen <zwijse@gmail.com>
2023-04-15 10:02:52 +0200

    initial commit

---
+++ ChatGPT.ps1
@@ -0,0 +1,72 @@
+﻿# Define API key and endpoint
+$ApiKey = "<your API key>"
+$ApiEndpoint = "https://api.openai.com/v1/chat/completions"
+
+# Define the system message. This sets the context for the AI.
+$AiSystemMessage = "You are a helpful assistant" # Default: "You are a helpful assistant"
+
+# Sets the initial system message
+Function Initialize-SystemMessage ($message){
+    $script:messages = @(
+        @{"role"="system"; "content"=$message}
+    )
+}
+
+# Function to send a message to ChatGPT
+function Invoke-ChatGPT ($messages) {
+    # Set the request headers
+    $headers = @{
+    "Content-Type" = "application/json"
+    "Authorization" = "Bearer $ApiKey"
+    }   
+
+    # Set the request body
+    $requestBody = @{
+        "model" = "gpt-3.5-turbo"
+        "messages" = $messages
+        "max_tokens" = 1000
+        "temperature" = 0.7
+    }
+
+    # Send the request
+    $response = Invoke-RestMethod -Method POST -Uri $ApiEndpoint -Headers $headers -Body (ConvertTo-Json $requestBody)
+
+    # Return the message content
+    return $response.choices[0].message.content
+}
+
+# Main loop
+
+Clear-Host
+Write-Host "######################`n# ChatGPT Powershell #`n######################`n`nEnter your prompt to continue. (type 'exit' to quit or 'reset' to start a new chat)" -ForegroundColor Yellow
+
+Initialize-SystemMessage $AiSystemMessage
+
+while ($true) {
+    # Capture user input
+    $userMessage = Read-Host "`nYou"
+
+    # Check if user wants to exit or reset
+    if ($userMessage -eq "exit") {
+        break
+    }
+    if ($userMessage -eq "reset") {
+        # Reset the message history so we can start with a clean slate
+        Initialize-SystemMessage $AiSystemMessage
+
+        Write-Host "Messages reset." -ForegroundColor Yellow
+        continue
+    }
+
+    # Add new user prompt to list of messages
+    $messages += @{"role"="user"; "content"=$userMessage}
+
+    # Query ChatGPT
+    $aiResponse = Invoke-ChatGPT $messages
+
+    # Show response
+    Write-Host "AI: $aiResponse" -ForegroundColor Yellow
+
+    # Add ChatGPT response to list of messages
+    $messages += @{"role"="assistant"; "content"=$aiResponse}
+}
--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# chatgpt-powershell
-example on how to use the chatGPT api with powershell
+# ChatGPT Powershell
+
+This is a simple PowerShell script that allows you to interact with OpenAI's ChatGPT in a conversational manner. The script provides a command-line interface for users to ask questions or provide prompts, and it will return ChatGPT's response.
+It serves as an example on how to use the chatGPT api with powershell
+
+## Requirements
+
+- PowerShell
+- An API key for OpenAI's API
+
+## Setup
+
+1. Replace `<your API key>` in the script with your OpenAI API key.
+2. Save the script as `ChatGPT-Powershell.ps1` or a name of your choice.
+
+## Usage
+
+1. Open PowerShell and navigate to the folder containing the script.
+2. Execute the script: `.\ChatGPT-Powershell.ps1`
+3. You'll see a welcome message and a prompt to enter your question or command.
+4. Type your question or command and press Enter.
+5. The AI will respond with its output.
+6. To exit the script, type `exit` and press Enter.
+7. To reset the conversation and start a new one, type `reset` and press Enter.
+
+## Example
+
+```plaintext
+######################
+# ChatGPT Powershell #
+######################
+Enter your prompt to continue. (type 'exit' to quit or 'reset' to start a new chat)
+
+You: What is the capital of France?
+AI: The capital of France is Paris.
+```
+
+## Notes
+
+- The script initializes the AI with a default system message: "You are a helpful assistant".
+- You can change this message to set a different context for the AI by modifying the `$AiSystemMessage` variable.
+- The script will keep track of the conversation history, so the AI can provide context-aware answers.
