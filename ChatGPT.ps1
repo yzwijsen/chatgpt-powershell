62c8496bb4b4ae78491ef223587dc0ac2fd8a3da
Yannick Zwijsen <zwijse@gmail.com>
2023-04-16 13:27:06 +0200

    refactor

--- ChatGPT.ps1
+++ ChatGPT.ps1
@@ -5,15 +5,16 @@ $ApiEndpoint = "https://api.openai.com/v1/chat/completions"
 # Define the system message. This sets the context for the AI.
 $AiSystemMessage = "You are a helpful assistant" # Default: "You are a helpful assistant"
 
+[System.Collections.Generic.List[Hashtable]]$MessageHistory = @()  # Message history
+
 # Sets the initial system message
-Function Initialize-SystemMessage ($message){
-    $script:messages = @(
-        @{"role"="system"; "content"=$message}
-    )
+Function Initialize-MessageHistory ($message){
+    $script:messages.Clear()
+    $script:messages.Add(@{"role" = "system"; "content" = $message}) | Out-Null
 }
 
 # Function to send a message to ChatGPT
-function Invoke-ChatGPT ($messages) {
+function Invoke-ChatGPT ($MessageHistory) {
     # Set the request headers
     $headers = @{
     "Content-Type" = "application/json"
@@ -23,7 +24,7 @@ function Invoke-ChatGPT ($messages) {
     # Set the request body
     $requestBody = @{
         "model" = "gpt-3.5-turbo"
-        "messages" = $messages
+        "messages" = $MessageHistory
         "max_tokens" = 1000
         "temperature" = 0.7
     }
@@ -40,7 +41,7 @@ function Invoke-ChatGPT ($messages) {
 Clear-Host
 Write-Host "######################`n# ChatGPT Powershell #`n######################`n`nEnter your prompt to continue. (type 'exit' to quit or 'reset' to start a new chat)" -ForegroundColor Yellow
 
-Initialize-SystemMessage $AiSystemMessage
+Initialize-MessageHistory $AiSystemMessage
 
 while ($true) {
     # Capture user input
@@ -52,21 +53,21 @@ while ($true) {
     }
     if ($userMessage -eq "reset") {
         # Reset the message history so we can start with a clean slate
-        Initialize-SystemMessage $AiSystemMessage
+        Initialize-MessageHistory $AiSystemMessage
 
         Write-Host "Messages reset." -ForegroundColor Yellow
         continue
     }
 
     # Add new user prompt to list of messages
-    $messages += @{"role"="user"; "content"=$userMessage}
+    $MessageHistory.Add(@{"role"="user"; "content"=$userMessage})
 
     # Query ChatGPT
-    $aiResponse = Invoke-ChatGPT $messages
+    $aiResponse = Invoke-ChatGPT $MessageHistory
 
     # Show response
     Write-Host "AI: $aiResponse" -ForegroundColor Yellow
 
     # Add ChatGPT response to list of messages
-    $messages += @{"role"="assistant"; "content"=$aiResponse}
+    $MessageHistory.Add(@{"role"="assistant"; "content"=$aiResponse})
 }
